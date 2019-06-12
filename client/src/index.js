90a3d8100c509d2226a5ca81fa9757168bfc765f
Eduard Tamsa <eduard.t@skyx.com>
2019-06-12 22:53:53 +0300

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,7 @@ ReactDOM.render(
         </BrowserRouter>
     </Provider>
     , document.querySelector('#root'));
-
+//...
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
